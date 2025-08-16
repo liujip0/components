5605136a24cff92b6c94880b3c653a84f15ba315
liujip0 <liujip2020@gmail.com>
2025-08-16 01:10:54 -0400

    organize props for snackbar

--- lib/components/Snackbar/Snackbar.tsx
+++ lib/components/Snackbar/Snackbar.tsx
@@ -3,17 +3,27 @@ import styles from "./Snackbar.module.css";
 
 type SnackbarProps = {
   open: boolean;
-  autoHideDuration?: number;
   onClose?: () => void;
+  autoHideDuration?: number;
+
   message?: string;
   action?: React.ReactNode;
+
+  className?: string;
+  messageClassName?: string;
+  actionClassName?: string;
 };
 export function Snackbar({
   open,
-  autoHideDuration,
   onClose,
+  autoHideDuration,
+
   message,
   action,
+
+  className,
+  messageClassName,
+  actionClassName,
 }: SnackbarProps) {
   useEffect(() => {
     if (open && autoHideDuration && onClose) {
@@ -25,9 +35,11 @@ export function Snackbar({
   }, [open, autoHideDuration, onClose]);
 
   return open ?
-      <div className={styles.snackbar}>
-        <p className={styles.message}>{message}</p>
-        <div>{action}</div>
+      <div className={styles.snackbar + " " + (className || "")}>
+        <p className={styles.message + " " + (messageClassName || "")}>
+          {message}
+        </p>
+        <div className={actionClassName || ""}>{action}</div>
       </div>
     : <></>;
 }
